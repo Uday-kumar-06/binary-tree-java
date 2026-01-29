600b1716ee140503accf3499ccb1e2e6ce203ea1
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-01-29 22:50:01 +0530

    Just BinaryTree code

--- Trees/src/main/java/introduction/BinaryTree.java
+++ Trees/src/main/java/introduction/BinaryTree.java
@@ -22,26 +22,54 @@ public class BinaryTree {
     }
 
     private void populate(Scanner scanner, Node node){
-        System.out.println("Do you want to enter left of" + node.value);
+        System.out.println("Do you want to enter left of " + node.value);
         boolean left = scanner.nextBoolean();
         if(left){
-            System.out.println("Enter the value" + node.value);
+            System.out.println("Enter the left value of " + node.value);
             int value = scanner.nextInt();
             node.left = new Node(value);
             populate(scanner,node.left);
         }
-        System.out.println("Do you want to enter right of" + node.value);
+        System.out.println("Do you want to enter right of " + node.value);
         boolean right = scanner.nextBoolean();
-        if(left){
-            System.out.println("Enter the value" + node.value);
+        if(right){
+            System.out.println("Enter the right value of " + node.value);
             int value = scanner.nextInt();
             node.right = new Node(value);
             populate(scanner,node.right);
         }
     }
 
+    public void display(){
+        display(root, "");
+    }
+    private void display(Node node, String indent){
+        if(node == null){
+            return;
+        }
+        System.out.println(indent+ node.value);
+        display(node.left, indent + "\t");
+        display(node.right, indent + "\t");
+    }
+    public void prettyDisplay() {
+        prettyDisplay(root, 0);
+    }
 
-    public static void main(String [] args){
+    private void prettyDisplay(Node node, int level) {
+        if (node == null) {
+            return;
+        }
 
+        prettyDisplay(node.right, level + 1);
+
+        if (level != 0) {
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t\t");
+            }
+            System.out.println("|------->" + node.value);
+        } else {
+            System.out.println(node.value);
+        }
+        prettyDisplay(node.left, level + 1);
     }
 }
