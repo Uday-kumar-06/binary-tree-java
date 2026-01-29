dace73e741e5647fc5648043305e18bd4b4f0c06
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-01-29 22:26:46 +0530

    Just Started to code

--- Trees/src/main/java/introduction/BinaryTree.java
+++ Trees/src/main/java/introduction/BinaryTree.java
@@ -1,6 +1,38 @@
 package introduction;
 
+import java.util.Scanner;
+
 public class BinaryTree {
+
+    private static class Node{
+        int value;
+        Node right;
+        Node left;
+        public Node(int value){
+            this.value = value;
+        }
+    }
+    private Node root;
+    //insert element
+    public void populate(Scanner scanner){
+        System.out.println("Enter the root Node: ");
+        int value = scanner.nextInt();
+        root = new Node(value);
+        populate(scanner, root);
+    }
+
+    private void populate(Scanner scanner, Node node){
+        System.out.println("Do you want to enter left of" + node.value);
+        boolean left = scanner.nextBoolean();
+        if(left){
+            System.out.println("Enter the value" + node.value);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            populate(scanner,node.left);
+        }
+    }
+
+
     public static void main(String [] args){
 
     }
